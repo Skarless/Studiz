41223ffe26b0029ea55dfeffef3be348164a34ac
Scar 3 <71671525+Skarless@users.noreply.github.com>
2022-10-25 18:05:50 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="mainContainer">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/MTCCfront.jpg" className="header_logo"  alt="logo"/>
+        <h1>
+            Studiz at IIT!
+        </h1>
+        <h2>Our pitch:</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Finding a spot to quietly study on campus is hard. With so many people attending IIT, there is a very high chance that you will be surrounded by noise upon finding your preferred spot. We want to find a way to eliminate this issue. Introducing Studiz, an easy to use web-app for finding and recommending a place to study on campus. Our app will suggest the user with a quiet place to study based on user submitted reviews of popular places on campus. These reviews will contain relevant information such as time stamps, locations, and approximate noise level. We will then develop an algorithm to sort through this information to find the best place for the user based on their input variables such as time and current location.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+            Developers: Spencer Albrecht, Tashi Bapu, John Chmura, Sakher Yaish, Eileen Garay, and Scarlett Kadan
+        </p>
     </div>
   );
 }
